aa9fc50b92f3a662bbd826e691ec9c67207ff1a1
Hwangwonuk <86584999+Hwangwonuk@users.noreply.github.com>
2022-03-07 00:20:51 +0900

    Mission03 Init

---
+++ mission03/src/test/java/dev/wonuk/mission03/Mission03ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wonuk.mission03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mission03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
