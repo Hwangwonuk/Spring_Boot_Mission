74d310d5728ed507d0ec96cec9524c84c6175cf6
Hwangwonuk <86584999+Hwangwonuk@users.noreply.github.com>
2022-03-19 22:17:27 +0900

    Mission 4 Basic

---
+++ Mission_4_Basic/src/main/java/dev/wonuk/community/service/CommunityUserDetailsService.java
@@ -0,0 +1,54 @@
+/*
+ * Created by Wonuk Hwang on 2022/03/19
+ * As part of Bigin
+ *
+ * Copyright (C) Bigin (https://bigin.io/main) - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by infra Team <wonuk_hwang@bigin.io>, 2022/03/19
+ */
+package dev.wonuk.community.service;
+
+import dev.wonuk.community.entity.UserEntity;
+import dev.wonuk.community.repository.UserRepository;
+import java.util.ArrayList;
+import java.util.Collection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+/**
+ * create on 2022/03/19. create by IntelliJ IDEA.
+ *
+ * <p> </p>
+ * <p> {@link } and {@link } </p> *
+ *
+ * @author wonukHwang
+ * @version 1.0
+ * @see
+ * @since (ex : 5 + 5)
+ */
+@Service
+public class CommunityUserDetailsService implements UserDetailsService {
+
+  private static final Logger logger = LoggerFactory.getLogger(CommunityUserDetailsService.class);
+  private final UserRepository userRepository;
+
+  public CommunityUserDetailsService(
+      @Autowired UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    final UserEntity userEntity = userRepository.findByUsername(username);
+    return new User(username, userEntity.getPassword(), new ArrayList<>());
+  }
+}
